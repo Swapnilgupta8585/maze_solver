fd04c81902147c2b22c9f90d3920e42c15404608
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-07-04 15:53:03 +0530

    add draw_move method of cell class

--- src/graphics.py
+++ src/graphics.py
@@ -120,3 +120,36 @@ class Cell:
         if self.has_left_wall:
             self._win.draw_line(
                 Line(Point(self._x1, self._y1), Point(self._x1, self._y2)), "red")
+
+    def draw_move(self, to_cell, undo=False):
+        """
+        Draw a move from the current cell to the target cell (`to_cell`).
+
+        The line will be drawn from the center of the current cell to the center of the target cell.
+        - If the `undo` flag is set to True, the line will be drawn in gray.
+        - Otherwise, the line will be drawn in red.
+
+        Assumptions:
+        - All cells are square.
+        - Coordinates are even numbers.
+
+        Steps to calculate the center of a cell:
+        1. `center_1_x` = left wall x-coordinate + half the distance between left and right walls.
+        2. `center_1_y` = top wall y-coordinate + half the distance between top and bottom walls.
+        3. Similarly, calculate `center_2` for the target cell.
+        """
+    
+        # Calculate center of the current cell
+        center_1 = Point((self._x1 + (self._x2 - self._x1)/2), (self._y1 + (self._y2 - self._y1)/2))
+        
+        # Calculate center of the target cell
+        center_2 = Point((to_cell._x1 + (to_cell._x2 - to_cell._x1)/2), (to_cell._y1 + (to_cell._y2 - to_cell._y1)/2))
+        
+        # Create a line from the center of the current cell to the center of the target cell
+        line = Line(center_1, center_2)
+        
+        # Draw the line in gray if undo is True, otherwise in red
+        if undo:
+            self._win.draw_line(line, "gray")
+        else:
+            self._win.draw_line(line, "red")
--- src/main.py
+++ src/main.py
@@ -2,10 +2,11 @@ from graphics import Window, Line, Point, Cell
 
 def main():
     win = Window(800,600)
-    cell = Cell(10,10,110,110,win,True,True,True,True)
-    cell2 = Cell(110,10,210,110,win)
+    cell = Cell(10,10,110,110,win,True,False,True,True)
+    cell2 = Cell(110,10,210,110,win,False)
     cell.draw()
     cell2.draw()
+    cell.draw_move(cell2,True)
     win.wait_for_close()
 
 main()

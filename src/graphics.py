3db1cda81f9a10e67b011e9bd820b3fdc71d888e
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-07-04 17:11:11 +0530

    update code

--- src/graphics.py
+++ src/graphics.py
@@ -1,6 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
 
-
 class Window:
     def __init__(self, width, height):
         # Create a root widget (window) where every other widget will reside.
@@ -11,7 +10,7 @@ class Window:
 
         # Initialize a Canvas widget with the root widget as its master.
         # The Canvas helps in drawing graphics and complex drawings.
-        self.__canvas = Canvas(self.__root, bg="#8b949e",
+        self.__canvas = Canvas(self.__root, bg="#343434",
                                height=height, width=width)
 
         # Pack the Canvas widget into the window (root widget).
@@ -81,75 +80,8 @@ class Line:
         )
 
 
-class Cell:
-    # The constructor takes x1,y1(top-left-corner) and x2,y2(bottom-right-corner)
-    # and a window instance and whether the walls exists or not
-    def __init__(self,
-                 x1,
-                 y1,
-                 x2,
-                 y2,
-                 window,
-                 has_left_wall=True,
-                 has_right_wall=True,
-                 has_top_wall=True,
-                 has_bottom_wall=True
-                 ):
-        self.has_left_wall = has_left_wall
-        self.has_right_wall = has_right_wall
-        self.has_top_wall = has_top_wall
-        self.has_bottom_wall = has_bottom_wall
-        self._x1 = x1
-        self._x2 = x2
-        self._y1 = y1
-        self._y2 = y2
-        self._win = window
 
-    def draw(self):
-        # This method just checks if a wall exists or not
-        # If they exist simply draw that wall on canvas through draw_line method of window class
-        if self.has_bottom_wall:
-            self._win.draw_line(
-                Line(Point(self._x1, self._y2), Point(self._x2, self._y2)), "red")
-        if self.has_right_wall:
-            self._win.draw_line(
-                Line(Point(self._x2, self._y1), Point(self._x2, self._y2)), "red")
-        if self.has_top_wall:
-            self._win.draw_line(
-                Line(Point(self._x1, self._y1), Point(self._x2, self._y1)), "red")
-        if self.has_left_wall:
-            self._win.draw_line(
-                Line(Point(self._x1, self._y1), Point(self._x1, self._y2)), "red")
 
-    def draw_move(self, to_cell, undo=False):
-        """
-        Draw a move from the current cell to the target cell (`to_cell`).
 
-        The line will be drawn from the center of the current cell to the center of the target cell.
-        - If the `undo` flag is set to True, the line will be drawn in gray.
-        - Otherwise, the line will be drawn in red.
-
-        Assumptions:
-        - All cells are square.
-        - Coordinates are even numbers.
-
-        Steps to calculate the center of a cell:
-        1. `center_1_x` = left wall x-coordinate + half the distance between left and right walls.
-        2. `center_1_y` = top wall y-coordinate + half the distance between top and bottom walls.
-        3. Similarly, calculate `center_2` for the target cell.
-        """
     
-        # Calculate center of the current cell
-        center_1 = Point((self._x1 + (self._x2 - self._x1)/2), (self._y1 + (self._y2 - self._y1)/2))
-        
-        # Calculate center of the target cell
-        center_2 = Point((to_cell._x1 + (to_cell._x2 - to_cell._x1)/2), (to_cell._y1 + (to_cell._y2 - to_cell._y1)/2))
-        
-        # Create a line from the center of the current cell to the center of the target cell
-        line = Line(center_1, center_2)
-        
-        # Draw the line in gray if undo is True, otherwise in red
-        if undo:
-            self._win.draw_line(line, "gray")
-        else:
-            self._win.draw_line(line, "red")
+
--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
-from graphics import Window, Line, Point, Cell
+from graphics import Window
+from maze import Maze
 
 def main():
     win = Window(800,600)
-    cell = Cell(10,10,110,110,win,True,False,True,True)
-    cell2 = Cell(110,10,210,110,win,False)
-    cell.draw()
-    cell2.draw()
-    cell.draw_move(cell2,True)
+    maze = Maze(550, 200, 10, 10, 80, 80, win)
     win.wait_for_close()
 
 main()

1a32b2dc2444ce8bfc651a45494e211c48e728cf
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-07-07 12:10:26 +0530

    update some code

--- src/cell.py
+++ src/cell.py
@@ -14,6 +14,7 @@ class Cell:
         self._y1 = None
         self._y2 = None
         self._win = window
+        self._visited = False
 
     def draw(self, x1, y1, x2, y2):
         # This method just checks if a wall exists or not

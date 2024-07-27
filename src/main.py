588b96fccd73d7c44676840eb594385fef9e5e2c
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-07-27 11:45:09 +0530

    add comments

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
-from graphics import Window
-from maze import Maze
+from graphics import Window  
+from maze import Maze        
 
 def main():
-    win = Window(800,600)
+    # Create a new window with a width of 800 pixels and a height of 600 pixels
+    win = Window(800, 600)
+    
+    # Initialize a Maze object with specified parameters
+    # Parameters: width, height, cell_width, cell_height, start_x, start_y, window
     maze = Maze(550, 200, 20, 20, 40, 40, win)
+    
+    # Solve the maze
     maze.solve()
+    
+    # Wait for the user to close the window before exiting the program
     win.wait_for_close()
 
+# Call the main function to execute the program
 main()
